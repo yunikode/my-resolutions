b5439cfd456f0dea073f03c4d78c8d3e021ac0fd
zee-german <franz.lutzenberger@gmail.com>
2016-05-03 10:50:16 +0200

    form to add new resolutions started

--- App.jsx
+++ App.jsx
@@ -1,9 +1,37 @@
 import React from 'react'
 
+Resolutions = new Mongo.Collection("resolutions")
+
 export default class App extends React.Component {
+
+  addResolution(e) {
+
+    e.preventDefault()
+
+    var text = this.refs.resolution.value.trim()
+
+    Resolutions.insert({
+      text: text,
+      complete: false,
+      createdAt: new Date()
+    })
+
+    this.refs.resolution.value = ""
+
+    console.log(text)
+  }
+
   render() {
     return (
-      <h1>Hello World</h1>
+      <div>
+        <h1>My Resolutions</h1>
+        <form className="new-resolution" onSubmit={this.addResolution.bind(this)}>
+          <input
+            type="text"
+            ref="resolution"
+            placeholder="Finish React Meteor Series"/>
+        </form>
+      </div>
     )
   }
 }
