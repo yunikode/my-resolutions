94cb1cb499541490a09bd12a4d8feee016be8421
zee-german <franz.lutzenberger@gmail.com>
2016-05-03 15:09:42 +0200

    everything gets a component

--- App.jsx
+++ App.jsx
@@ -1,27 +1,15 @@
 import React from 'react'
 import TrackerReact from 'meteor/ultimatejs:tracker-react'
 
+import ResolutionsForm from './ResolutionsForm'
+import ResolutionSingle from './ResolutionSingle'
+
 Resolutions = new Mongo.Collection("resolutions")
 
 export default class App extends TrackerReact(React.Component) {
 
   resolutions() {
     return Resolutions.find().fetch()
-  }
-
-  addResolution(e) {
-
-    e.preventDefault()
-
-    var text = this.refs.resolution.value.trim()
-
-    Resolutions.insert({
-      text: text,
-      complete: false,
-      createdAt: new Date()
-    })
-
-    this.refs.resolution.value = ""
   }
 
   render() {
@@ -34,15 +22,10 @@ export default class App extends TrackerReact(React.Component) {
     return (
       <div>
         <h1>My Resolutions</h1>
-        <form className="new-resolution" onSubmit={this.addResolution.bind(this)}>
-          <input
-            type="text"
-            ref="resolution"
-            placeholder="Finish React Meteor Series"/>
-        </form>
-        <div>
-          {res[0].text}
-        </div>
+        <ResolutionsForm />
+        <ul>
+          <ResolutionSingle resolution={ res[0] } />
+        </ul>
       </div>
     )
   }
---
+++ ResolutionsForm.jsx
@@ -0,0 +1,29 @@
+import React, { Component } from 'react'
+
+export default class ResolutionsForm extends Component {
+
+  addResolution(e) {
+    e.preventDefault()
+
+    var text = this.refs.resolution.value.trim()
+
+    Resolutions.insert({
+      text: text,
+      complete: false,
+      createdAt: new Date()
+    })
+
+    this.refs.resolution.value = ""
+  }
+
+  render() {
+    return (
+      <form className="new-resolution" onSubmit={this.addResolution.bind(this)}>
+        <input
+          type="text"
+          ref="resolution"
+          placeholder="Finish React Meteor Series"/>
+      </form>
+    )
+  }
+}
